74452855e644e134b76d7afaa277c442c94c7a92
머홍 <deahong1214@gmail.com>
2025-12-28 14:46:37 +0900

    깃허브 페이지 업로드를 위해 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  
+  images: {
+    unoptimized: true,
+  },
+  
+  basePath: '/webp2png', 
 };
 
 export default nextConfig;
